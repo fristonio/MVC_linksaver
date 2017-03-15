1e782a32e2a56f43a29cf3ec5f306f7cc53a800e
fristonio <deepshpathak@gmail.com>
2017-03-15 12:09:11 +0530

    Completed Login part

--- app/models/Login.php
+++ app/models/Login.php
@@ -1,15 +1,26 @@
 <?php 
 	
-	global $CONFIG;
 	namespace Link\Models;
 
 	class Login {
 		public static function DB(){
-			return new PDO("mysql:host:$CONFIG["dbhost"];dbname=$CONFIG["dbname"]",$CONFIG["dbuser"], $CONFIG["dbpass"]);
+			global $CONFIG;
+			return new \PDO("mysql:host={$CONFIG['dbhost']};dbname={$CONFIG['dbname']}",$CONFIG['dbuser'], $CONFIG['dbpass']);
 		}
 		
 		public function validateUser( $userData ) {
-			$conn = self::DB();
+			try {
+				$conn = self::DB();
+				$conn->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+				$query = "SELECT * FROM users WHERE email=:email and password=:pass";
+				$stmt = $conn->prepare($query);
+				$stmt->execute($userData);
+				return $stmt->fetch(\PDO::FETCH_ASSOC);
+			}
+			catch (\PDOException $e) {
+				echo $e->getMessage();
+				return false;
+			}
 			
 		} 
 	}
